5671c87973ea80139aa4382ebc1d443b87fbcc32
AKRAM <akramloukrimy@gmail.com>
2024-04-01 22:03:01 -0700

    if-else statements in python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def remove_char_at(s, n):
-    if n < 0 or n >= len(s):
-        return "Index out of range"
-    
-    result = ''
-    for i in range(len(s)):
-        if i != n:
-            result += s[i]
-    return result
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
 
